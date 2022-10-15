63657a65b438b80117958c330b5743a11dd8c733
LeeShinYeoung <iqrash12@gmail.com>
2022-10-16 01:50:46 +0900

    Add generateBackgroundCanvas

--- src/pixel-painter.ts
+++ src/pixel-painter.ts
@@ -1,6 +1,7 @@
 class PixelPainter {
   private drawCanvas: HTMLCanvasElement
   private drawCanvasContext: CanvasRenderingContext2D
+  private backgroundCanvas: HTMLCanvasElement
   private columnCount: number
   private rowCount: number
   private cellWidth: number
@@ -26,7 +27,10 @@ class PixelPainter {
     this.cellWidth = this.drawCanvas.width / this.columnCount
     this.cellHeight = this.drawCanvas.height / this.rowCount
 
+    this.backgroundCanvas = this.generateBackgroundCanvasElement()
+
     this.wrapper.appendChild(this.drawCanvas)
+    this.wrapper.appendChild(this.backgroundCanvas)
   }
 
   generateDrawCanvasElement() {
@@ -38,5 +42,37 @@ class PixelPainter {
     canvas.width = this.wrapper.clientWidth
     canvas.height = this.wrapper.clientHeight
     return canvas
+  }
+
+  generateBackgroundCanvasElement() {
+    const backgroundCanvas = document.createElement('canvas')
+    this.wrapper.style.position = 'relative'
+    backgroundCanvas.style.position = 'absolute'
+    backgroundCanvas.style.zIndex = '-1'
+    backgroundCanvas.style.top = '0'
+    backgroundCanvas.style.left = '0'
+    backgroundCanvas.style.width = '100%'
+    backgroundCanvas.style.height = '100%'
+    backgroundCanvas.width = this.wrapper.clientWidth
+    backgroundCanvas.height = this.wrapper.clientHeight
+    this.wrapper.appendChild(backgroundCanvas)
+
+    const backgroundCanvasContext = backgroundCanvas.getContext('2d') as CanvasRenderingContext2D
+
+    for (let y = 0; y < this.rowCount; y++) {
+      for (let x = 0; x < this.columnCount; x++) {
+        const cellIndex = x * this.columnCount + y
+        const cellColor = cellIndex % 2 === 0 ? '#e6e6e6' : '#f8f8f8'
+        backgroundCanvasContext.fillStyle = cellColor
+        backgroundCanvasContext.fillRect(
+          x * this.cellWidth,
+          y * this.cellHeight,
+          this.cellWidth,
+          this.cellHeight
+        )
+      }
+    }
+
+    return backgroundCanvas
   }
 }
