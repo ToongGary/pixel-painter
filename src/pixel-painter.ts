4fae67f16f55b4134d22121c0e159f306437a7f1
LeeShinYeoung <iqrash12@gmail.com>
2022-10-20 00:40:55 +0900

    Add mouse event and make color selection function

--- src/pixel-painter.ts
+++ src/pixel-painter.ts
@@ -2,10 +2,15 @@ class PixelPainter {
   private drawCanvas: HTMLCanvasElement
   private drawCanvasContext: CanvasRenderingContext2D
   private backgroundCanvas: HTMLCanvasElement
+  private colorsDiv: HTMLDivElement
   private columnCount: number
   private rowCount: number
   private cellWidth: number
   private cellHeight: number
+
+  private isClick: boolean
+
+  private cellColor: string
 
   private wrapper: HTMLDivElement
 
@@ -18,6 +23,8 @@ class PixelPainter {
     columnCount: number
     rowCount: number
   }) {
+    this.cellColor = 'black'
+    this.isClick = false
     this.wrapper = document.getElementById(wrapperId) as HTMLDivElement
     this.columnCount = columnCount
     this.rowCount = rowCount
@@ -29,8 +36,13 @@ class PixelPainter {
 
     this.backgroundCanvas = this.generateBackgroundCanvasElement()
 
-    this.wrapper.appendChild(this.drawCanvas)
-    this.wrapper.appendChild(this.backgroundCanvas)
+    this.colorsDiv = this.generateColorsElement()
+
+    const div = document.createElement('div')
+    div.appendChild(this.backgroundCanvas)
+    div.appendChild(this.drawCanvas)
+    this.wrapper.appendChild(div)
+    this.wrapper.appendChild(this.colorsDiv)
 
     this.initializeHandleEvents()
   }
@@ -78,27 +90,109 @@ class PixelPainter {
     return backgroundCanvas
   }
 
-  initializeHandleEvents() {
-    this.drawCanvas.addEventListener('mousedown', this.handleCanvasClick.bind(this))
+  generateColorsElement() {
+    const colors = ['black', 'red', 'blue', 'green', 'yellow']
+
+    const colorWrapperDiv = document.createElement('div')
+    colorWrapperDiv.style.display = 'flex'
+    colorWrapperDiv.style.flexFlow = 'row nowrap'
+    colorWrapperDiv.style.marginTop = '30px'
+
+    colors.forEach((color) => {
+      const colorDiv = document.createElement('div')
+      colorDiv.style.width = '30px'
+      colorDiv.style.height = '30px'
+      colorDiv.style.marginRight = '3px'
+      colorDiv.style.backgroundColor = color
+      colorDiv.style.cursor = 'pointer'
+
+      const attributes = document.createAttribute('data-color')
+      attributes.value = color
+      colorDiv.setAttributeNode(attributes)
+      colorWrapperDiv.appendChild(colorDiv)
+    })
+
+    const label = document.createElement('label')
+    label.style.display = 'block'
+    label.style.width = '30px'
+    label.style.height = '30px'
+    label.style.backgroundColor = 'skyblue'
+    label.style.position = 'relative'
+    const input = document.createElement('input')
+    input.type = 'color'
+    input.style.position = 'absolute'
+    input.style.visibility = 'hidden'
+    input.style.width = '100%'
+    input.style.display = 'block'
+    input.style.height = '100%'
+    label.appendChild(input)
+
+    colorWrapperDiv.appendChild(label)
+
+    return colorWrapperDiv
   }
 
-  handleCanvasClick(event: MouseEvent) {
+  initializeHandleEvents() {
+    this.drawCanvas.addEventListener('mousedown', this.handleCanvasMousedown.bind(this))
+    this.drawCanvas.addEventListener('mouseup', this.handleCanvasMouseup.bind(this))
+    this.drawCanvas.addEventListener('contextmenu', (event) => event.preventDefault())
+    this.drawCanvas.addEventListener('mousemove', this.handleCanvasMouseMove.bind(this))
+    this.colorsDiv.addEventListener('mousedown', this.handleColorMousedown.bind(this))
+  }
+
+  handleColorMousedown(event: MouseEvent) {
+    const target = event.target as HTMLDivElement
+    const color = target.dataset.color as string
+    this.cellColor = color
+  }
+
+  handleCanvasMousedown(event: MouseEvent) {
+    this.isClick = true
+    const { x, y } = this.getCellCoordinate(event.clientX, event.clientY)
+
+    if (event.which === 3) {
+      this.removeCell(x, y)
+    } else {
+      this.fillCell(x, y)
+    }
+  }
+
+  handleCanvasMouseup(event: MouseEvent) {
+    this.isClick = false
+  }
+
+  handleCanvasMouseMove(event: MouseEvent) {
+    if (!this.isClick) return
+    const { x, y } = this.getCellCoordinate(event.clientX, event.clientY)
+
+    if (event.which === 3) {
+      this.removeCell(x, y)
+    } else {
+      this.fillCell(x, y)
+    }
+  }
+
+  getCellCoordinate(clientX: number, clientY: number) {
     const { left: canvasLeft, top: canvasTop } = this.drawCanvas.getBoundingClientRect()
-    const x = event.clientX - canvasLeft
-    const y = event.clientY - canvasTop
+    const x = clientX - canvasLeft
+    const y = clientY - canvasTop
 
     const cellX = Math.floor(x / this.cellWidth)
     const cellY = Math.floor(y / this.cellHeight)
 
-    this.fillCell(cellX, cellY)
+    return { x: cellX, y: cellY }
   }
 
   fillCell(cellX: number, cellY: number) {
-    this.drawCanvasContext.fillStyle = 'black'
-
     const startX = cellX * this.cellWidth
     const startY = cellY * this.cellHeight
-
+    this.drawCanvasContext.fillStyle = this.cellColor
     this.drawCanvasContext.fillRect(startX, startY, this.cellWidth, this.cellWidth)
+  }
+
+  removeCell(cellX: number, cellY: number) {
+    const startX = cellX * this.cellWidth
+    const startY = cellY * this.cellHeight
+    this.drawCanvasContext.clearRect(startX, startY, this.cellWidth, this.cellHeight)
   }
 }
